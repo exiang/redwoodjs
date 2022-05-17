db5866b62b96de6258de2cb319b8005f836170ae
Yee Siang <exiang83@yahoo.com>
2022-05-17 16:27:58 +0000

    crud post done

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,11 +7,25 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
+import PostsLayout from 'src/layouts/PostsLayout'
+import BlogLayout from 'src/layouts/BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Route path="/article" page={ArticlePage} name="article" />
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
+      <Set wrap={BlogLayout}>
+        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
---
+++ web/src/pages/ArticlePage/ArticlePage.tsx
@@ -0,0 +1,18 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+import ArticleCell from 'src/components/ArticleCell'
+
+interface Props {
+  id: number
+}
+const ArticlePage = ({ id }: Props) => {
+  return (
+    <>
+      <MetaTags title="Article" description="Article page" />
+
+      <ArticleCell id={id} rand={Math.random()} />
+    </>
+  )
+}
+
+export default ArticlePage
